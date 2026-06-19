4ae0f7b059a0e98818fd2aa00033a1445b42bdc5
Martin <marti_borislavov@abv.bg>
2026-06-20 01:44:32 +0300

    feat: Added IndexController, SessionInterceptor, WebMvcConfiguration, UserRegisterRequest, UserLoginRequest, UserDto, UserMapper, added to User Service register and login methods.
    chore: Added findByEmail method to UserRepository and changed the index.html file

---
+++ src/main/java/app/service/user/UserService.java
@@ -0,0 +1,52 @@
+package app.service.user;
+
+import app.mapper.user.UserMapper;
+import app.model.dto.user.UserDto;
+import app.model.dto.user.UserLoginRequest;
+import app.model.dto.user.UserRegisterRequest;
+import app.model.entity.user.User;
+import app.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserDto login(UserLoginRequest userLoginRequest) {
+        Optional<User> optionalUser =  userRepository.findByEmail(userLoginRequest.getEmail());
+
+        if(optionalUser.isEmpty() ||
+                !passwordEncoder.matches(userLoginRequest.getPassword(),optionalUser.get().getPassword())) {
+            throw new RuntimeException("Email or password mismatch");
+        }
+
+        return UserMapper.toUserDto(optionalUser.get());
+    }
+
+    public UserDto register(UserRegisterRequest userRegisterRequest) {
+        userRepository.findByEmail(userRegisterRequest.getEmail()).ifPresent(user -> {
+            throw new RuntimeException("User with this email already exists!");
+        });
+
+        String encodedPassword = passwordEncoder.encode(userRegisterRequest.getPassword());
+        userRegisterRequest.setPassword(encodedPassword);
+
+        User userEntity = UserMapper.toUserEntity(userRegisterRequest);
+
+        userRepository.save(userEntity);
+
+        return UserMapper.toUserDto(userEntity);
+    }
+}
